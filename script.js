2e75d14f743ea75d5cf421753331fc3c341ccfa0
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-06-14 21:58:49 -0300

    completed desafio

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ for (let numero = 1; numero <= 10; numero +=1){
+     console.log (numero)
+}
+console.log ("")
+  let numerinho = 10
+  while( 1 <= numerinho){
+      console.log (numerinho)
+      numerinho--
+}
+console.log ("")
+ for (let count = 1; count <=100; count +=2){
+     console.log (count)
+}
+console.log ("")
+ for (let number = 2; number <=100; number += 2){
+     console.log (number)
+}
